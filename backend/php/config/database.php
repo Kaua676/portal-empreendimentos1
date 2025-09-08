27d463d7ec1742bfc038bd7cf54faea61894f226
Kaua Vicente Domingos <kauavic676@gmail.com>
2025-09-08 14:36:21 -0400

    feat: credencials / docs: readme

--- backend/php/config/database.php
+++ backend/php/config/database.php
@@ -1,17 +1,15 @@
 <?php
-// conexao.php
+require_once __DIR__ . '/env.php';
 
-$host = 'localhost:3306';  // Ou o IP do seu servidor de banco
-$user = 'kadom_admin';       // Usuário do banco
-$pass = 'M8OkAl&7jcna7px#';           // Senha do banco
-$db   = 'kadom_portalemp';    // Nome do banco
-
-$mysqli = new mysqli($host, $user, $pass, $db);
+$mysqli = new mysqli(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_NAME']
+);
 
 if ($mysqli->connect_errno) {
     die("Falha na conexão com o banco de dados: " . $mysqli->connect_error);
 }
 
-// Se quiser usar charset UTF-8:
 $mysqli->set_charset("utf8");
-?>
